ca40449186c4a86a10f61f93bedf51810f500e59
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2024-05-23 10:45:33 -0500

    app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EstudianteModule } from './estudiante/estudiante.module';
 import { ProfesorService } from './profesor/profesor.service';
 import { ProyectoService } from './proyecto/proyecto.service';
 import { PropuestaService } from './propuesta/propuesta.service';
 import { EstudianteService } from './estudiante/estudiante.service';
+import { Profesor } from './profesor/profesor.entity';
+import { Propuesta } from './propuesta/propuesta.entity';
+import { Proyecto } from './proyecto/proyecto.entity';
+import { Estudiante } from './estudiante/estudiante.entity';
 
 @Module({
-  imports: [EstudianteModule],
+  imports: [
+    TypeOrmModule.forFeature([Profesor, Propuesta, Proyecto, Estudiante]),
+  ],
   controllers: [AppController],
-  providers: [AppService, ProfesorService, ProyectoService, PropuestaService, EstudianteService],
+  providers: [
+    AppService,
+    ProfesorService,
+    ProyectoService,
+    PropuestaService,
+    EstudianteService,
+  ],
 })
 export class AppModule {}
